c57204398d9a01a6711537d18e4372732e2bf18f
Gentile, John <johncgentile17@gmail.com>
2022-01-15 09:12:24 -0500

    start of binary search

---
+++ algorithms/README.md
@@ -0,0 +1,6 @@
+# Algorithms
+
+## References
+
+* [Algorithms, 4th Edition - Robert Sedgewick and Kevin Wayne](http://amzn.to/13VNJi7)
+
---
+++ algorithms/search/binary_search.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Using Python type hints (ver. >=3.5): https://docs.python.org/3/library/typing.html
+def binary_search(key: int, sorted_collection: "list[int]") -> int:
+	""" Traditional Binary Search algorithm in pure Python
+
+	**Note:** `sorted_collection` must be in ascending order.
+
+	:param key: integer value to search for
+	:param sorted_collection: pre-sorted list of integers
+	:return: index of key in list, or -1 if not found
+	"""
+	lo = 0
+	hi = len(sorted_collection) - 1
+
+	while lo <= hi:
+		# NOTE: remember, /2 -> floating pt. return, //2 -> integer return
+		mid = lo + (hi - lo) // 2
+		if key < sorted_collection[mid]:
+			hi = mid - 1
+		elif key > sorted_collection[mid]:
+			lo = mid + 1
+		else:
+			return mid
+	# key not found (for Python >3.10, return type can be None here to be
+	# "more Pythonic", and return type alias woulc be "-> int | None:"
+	return -1
+
+if __name__ == "__main__":
+	print("Hello")
+	print(binary_search(42, [0, 1, 4, 5, 41, 42, 43, 55]))
+
